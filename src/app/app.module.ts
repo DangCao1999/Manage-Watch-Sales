a83c5a2d299ef491877fac6d2a4e925d6422a913
cao.nvd@itsstraining.edu.vn <cao.nvd@itsstraining.edu.vn>
2020-06-03 07:50:11 +0700

    add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,29 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
+import { LoginComponent } from './components/login/login.component';
+import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+
+const routes: Routes = [
+  {path: '', component: LoginComponent},
+  {path: '/login', component: LoginComponent},
+  {path: '/dashboard', component: DashboardComponent},
+];
 @NgModule({
   declarations: [
     AppComponent,
+    LoginComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatButtonModule,
     MatFormFieldModule,
     MatInputModule,
-    
+
   ],
   providers: [],
   bootstrap: [AppComponent]
